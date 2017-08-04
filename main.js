15a1d3c42e29e2f8456c073664c48157f84912e1
Isaac Hardy <isaac@ihardy.co>
2017-08-04 16:20:53 -0400

    second commit

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Blackjack</title>
-  <link rel="stylesheet" href="main.css">
-</head>
-<body>
-  <h1>MY CALCULATOR</h1>
-  <h4></h4>
+let calculations =[]
 
-  <script src="/main.js"></script>
-  <script src=".js"></script>
-</body>
-</html>
+
+let calcWindow = document.getElementById("blank");
+
+let sevenButton = document.getElementById('seven');
+
+
+sevenButton.addEventListener("click",function(){
+    calcWindow.innerHTML = '7';
+    // alert("seven is clicked");
+})

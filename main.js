cd7fdcc4f07203a530a407b038b06f08bb882260
Isaac Hardy <isaac@ihardy.co>
2017-08-04 14:03:46 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Blackjack</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+  <h1>MY CALCULATOR</h1>
+  <h4></h4>
+
+  <script src="/main.js"></script>
+  <script src=".js"></script>
+</body>
+</html>
